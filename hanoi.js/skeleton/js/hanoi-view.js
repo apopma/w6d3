7177cf800da48ae06808a5f768c08781b1fc78e0
App Academy Student <student@appacademy.io>
2015-07-29 16:25:06 -0700

    can render discs

--- hanoi.js/skeleton/js/hanoi-view.js
+++ hanoi.js/skeleton/js/hanoi-view.js
@@ -7,21 +7,37 @@
     this.game = game;
     this.$el = $el;
     this.setupTowers();
+    this.render();
   };
 
   View.prototype.setupTowers = function () {
     for (var i = 0; i < 3; i++) {
-      var $pile = $("<div>").addClass("pile");
+      var $pile = $("<div>").addClass("pile").addClass("pile-" + i);
       $pile.data("id", i);
 
-      if (i === 0) {
-        for (var j = 1; j <= 3; j++) {
-          var $disc = $("<div>").addClass("disc").addClass("size-" + j);
-          $pile.append($disc);
-        }
+      for (var j = 1; j <= 3; j++) {
+        var $disc = $("<div>").addClass("disc").addClass("empty");
+        $pile.append($disc);
       }
 
       this.$el.append($pile);
+    }
+  };
+
+  View.prototype.render = function() {
+    $(".disc").removeClass("size-1 size-2 size-3").addClass("empty");
+
+    for (var i = 0; i < this.game.towers.length; i++) {
+      var pile = this.game.towers[i];
+      var $pile = $(".pile-" + i);
+
+      var discIndex;
+      for (var j = 0; j < pile.length; j++) {
+        discIndex = 2 - j;
+        $pile.children().eq(discIndex)
+          .addClass("size-" + pile[j])
+          .removeClass("empty");
+      }
     }
   };
 })();
